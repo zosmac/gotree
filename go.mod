a26fe24217448f8a7dcc1d03671574181e22e002
zosmac <zosmac@users.noreply.github.com>
2023-01-31 23:27:16 -0500

    Add files via upload
    Initial content

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/zosmac/gotree
+
+go 1.19
+
+require github.com/zosmac/gocore v0.0.0-20230131192841-f203a6f0816a
+
+require (
+	github.com/StackExchange/wmi v1.2.1 // indirect
+	github.com/go-ole/go-ole v1.2.5 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/tools v0.5.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,272 @@
+// Copyright © 2022 The Gomon Project.
+
+package main
+
+/*
+#include <libproc.h>
+#include <sys/sysctl.h>
+*/
+import "C"
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"unsafe"
+
+	"github.com/zosmac/gocore"
+)
+
+type (
+	// Pid is the type for the process identifier.
+	Pid int
+
+	// processTable defines a process table as a map of pids to processes.
+	processTable map[Pid]*process
+
+	// processTree organizes the process into a hierarchy
+	processTree map[Pid]processTree
+
+	// process info.
+	process struct {
+		ancestors []Pid
+		Pid
+		Ppid Pid
+		CommandLine
+	}
+
+	// CommandLine contains a process' command line arguments.
+	CommandLine struct {
+		Executable string   `json:"executable" gomon:"property"`
+		Args       []string `json:"args" gomon:"property"`
+		Envs       []string `json:"envs" gomon:"property"`
+	}
+)
+
+// String renders the pid.
+func (pid Pid) String() string {
+	return strconv.Itoa(int(pid))
+}
+
+// main
+func main() {
+	gocore.Main(Main)
+}
+
+// Main builds and displays the process tree.
+func Main(ctx context.Context) {
+	pid := 1
+	if len(os.Args) > 1 {
+		pid, _ = strconv.Atoi(os.Args[1])
+	}
+	tb := buildTable()
+	tr := findTree(buildTree(tb), Pid(pid))
+	flatTree(tb, tr)
+}
+
+// getPids gets the list of active processes by pid.
+func getPids() ([]Pid, error) {
+	n, err := C.proc_listpids(C.PROC_ALL_PIDS, 0, nil, 0)
+	if n <= 0 {
+		return nil, gocore.Error("proc_listpids PROC_ALL_PIDS failed", err)
+	}
+
+	var pid C.int
+	buf := make([]C.int, n/C.int(unsafe.Sizeof(pid))+10)
+	if n, err = C.proc_listpids(C.PROC_ALL_PIDS, 0, unsafe.Pointer(&buf[0]), n); n <= 0 {
+		return nil, gocore.Error("proc_listpids PROC_ALL_PIDS failed", err)
+	}
+	n /= C.int(unsafe.Sizeof(pid))
+	if int(n) < len(buf) {
+		buf = buf[:n]
+	}
+
+	pids := make([]Pid, len(buf))
+	for i, pid := range buf {
+		pids[int(n)-i-1] = Pid(pid) // Darwin returns pids in descending order, so reverse the order
+	}
+	return pids, nil
+}
+
+// buildTable builds a process table and captures current process state.
+func buildTable() processTable {
+	pids, err := getPids()
+	if err != nil {
+		panic(fmt.Errorf("could not build process table %v", err))
+	}
+
+	tb := make(map[Pid]*process, len(pids))
+	for _, pid := range pids {
+
+		var bsi C.struct_proc_bsdshortinfo
+		if n := C.proc_pidinfo(
+			C.int(pid),
+			C.PROC_PIDT_SHORTBSDINFO,
+			0,
+			unsafe.Pointer(&bsi),
+			C.int(C.PROC_PIDT_SHORTBSDINFO_SIZE),
+		); n != C.int(C.PROC_PIDT_SHORTBSDINFO_SIZE) {
+			continue
+		}
+
+		tb[pid] = &process{
+			ancestors:   []Pid{},
+			Pid:         pid,
+			Ppid:        Pid(bsi.pbsi_ppid),
+			CommandLine: pid.commandLine(),
+		}
+	}
+
+	for pid, p := range tb {
+		p.ancestors = func() []Pid {
+			var pids []Pid
+			for pid := tb[pid].Ppid; pid > 0; pid = tb[pid].Ppid {
+				pids = append([]Pid{pid}, pids...)
+			}
+			return pids
+		}()
+	}
+
+	return tb
+}
+
+// buildTree builds the process tree.
+func buildTree(tb processTable) processTree {
+	tr := processTree{}
+
+	for pid, p := range tb {
+		addPid(tr, append(p.ancestors, pid))
+	}
+
+	return tr
+}
+
+// addPid adds a process into the tree.
+func addPid(tr processTree, ancestors []Pid) {
+	if len(ancestors) == 0 {
+		return
+	}
+	if _, ok := tr[ancestors[0]]; !ok {
+		tr[ancestors[0]] = processTree{}
+	}
+	addPid(tr[ancestors[0]], ancestors[1:])
+}
+
+// flatTree starts the display of the process tree.
+func flatTree(tb processTable, tr processTree) []Pid {
+	return flatTreeIndent(tb, tr, 0)
+}
+
+// flatTreeIndent recurses through the process tree to display hierarchy of processes.
+func flatTreeIndent(tb processTable, tr processTree, indent int) []Pid {
+	if len(tr) == 0 {
+		return nil
+	}
+	var flat []Pid
+
+	var pids []Pid
+	for pid := range tr {
+		pids = append(pids, pid)
+	}
+
+	sort.Slice(pids, func(i, j int) bool {
+		dti := depthTree(tr[pids[i]])
+		dtj := depthTree(tr[pids[j]])
+		return dti > dtj ||
+			dti == dtj && pids[i] < pids[j]
+	})
+
+	for _, pid := range pids {
+		flat = append(flat, pid)
+		display(tb[pid], indent)
+		flat = append(flat, flatTreeIndent(tb, tr[pid], indent+1)...)
+	}
+
+	return flat
+}
+
+// display shows the pid, command, arguments, and environment variables for a process.
+func display(p *process, indent int) {
+	tab := fmt.Sprintf("\n%s", strings.Repeat("\t|", indent))
+	pid := p.Pid.String()
+	pre := tab + "\033[97;40m" + "      "[:6-len(pid)] + pid
+	tab += "\t"
+	var cmd, args, envs string
+	if len(p.Args) > 0 {
+		cmd = p.Args[0]
+	}
+	if len(p.Args) > 1 {
+		args = "\033[m" + tab + "\033[34m" + strings.Join(p.Args[1:], "\033[m"+tab+"\033[34m")
+	}
+	if len(p.Envs) > 0 {
+		envs = "\033[m" + tab + "\033[35m" + strings.Join(p.Envs, "\033[m"+tab+"\033[35m")
+	}
+	fmt.Printf("%s\033[m  %s%s%s\033[m", pre, cmd, args, envs)
+}
+
+// depthTree enables sort of deepest process trees first.
+func depthTree(tr processTree) int {
+	depth := 0
+	for _, tree := range tr {
+		dt := depthTree(tree) + 1
+		if depth < dt {
+			depth = dt
+		}
+	}
+	return depth
+}
+
+// findTree finds the process tree parented by a specific process.
+func findTree(tr processTree, parent Pid) processTree {
+	for pid, tr := range tr {
+		if pid == parent {
+			return processTree{parent: tr}
+		}
+		if tr = findTree(tr, parent); tr != nil {
+			return tr
+		}
+	}
+
+	return nil
+}
+
+// commandLine retrieves process command, arguments, and environment.
+func (pid Pid) commandLine() CommandLine {
+	size := C.size_t(C.ARG_MAX)
+	buf := make([]byte, size)
+
+	if rv := C.sysctl(
+		(*C.int)(unsafe.Pointer(&[3]C.int{C.CTL_KERN, C.KERN_PROCARGS2, C.int(pid)})),
+		3,
+		unsafe.Pointer(&buf[0]),
+		&size,
+		unsafe.Pointer(nil),
+		0,
+	); rv != 0 {
+		return CommandLine{}
+	}
+
+	l := int(*(*uint32)(unsafe.Pointer(&buf[0])))
+	ss := bytes.FieldsFunc(buf[4:size], func(r rune) bool { return r == 0 })
+	var executable string
+	var args, envs []string
+	for i, s := range ss {
+		if i == 0 {
+			executable = string(s)
+		} else if i <= l {
+			args = append(args, string(s))
+		} else {
+			envs = append(envs, string(s))
+		}
+	}
+
+	return CommandLine{
+		Executable: executable,
+		Args:       args,
+		Envs:       envs,
+	}
+}
